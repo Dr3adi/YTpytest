07de96c6325a3b1ef4820a636ad0fa24a7b25c8e
Artsem Mukhin <artem.muxin@gmail.com>
2025-02-09 12:42:56 +0300

    pytest #5

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,15 @@ from configuration import SERVICE_URL
 def get_users():
     response = requests.get(SERVICE_URL)
     return response
+
+
+def _calculate(a, b):
+    if isinstance(a, int) and isinstance(b, int):
+        return a + b
+    else:
+        return None
+
+
+@pytest.fixture()
+def calculate():
+    return _calculate
--- tests/test_something.py
+++ tests/test_something.py
@@ -1,7 +1,24 @@
 from src.baseclasses.response import Response
 from src.schemas.user import User
+import pytest
 
 
 def test_getting_users(get_users):
     data = Response(get_users)
     data.assert_status_code(200).validate(User)
+
+
+@pytest.mark.skip('Заблокированно задачей OMNI-17456')
+def test_another():
+    assert 1 == 1
+
+
+@pytest.mark.dev
+@pytest.mark.parametrize('first_value, second_value, result', [
+    (1, 2, 3),
+    ('text1', 'text2', None),
+    ('text', 2, None),
+    (2, 'text', None)
+])
+def test_calculate(first_value, second_value, result, calculate):
+    assert calculate(first_value, second_value) == result
